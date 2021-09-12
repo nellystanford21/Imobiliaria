4264e4aed78f450abf77cb9d413ab649616126fc
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-09-12 00:58:50 -0300

    .h

---
+++ projeto/registraTerreno.h
@@ -0,0 +1,13 @@
+#pragma once 
+#include <string>
+
+using namespace std;
+
+class registraTerreno {
+    private:
+        string tituloAnuncio;
+        float area;
+
+    public:
+        void cadastraTerreno();
+};
