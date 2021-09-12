d4e2aa45ad21bf27be6e5b5fd85fabed4d897f3a
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-09-12 00:58:14 -0300

    .h

---
+++ projeto/registraCasa.h
@@ -0,0 +1,16 @@
+#pragma once 
+#include <string>
+
+using namespace std;
+
+class registraCasa {
+    private:
+        string tituloAnuncio;
+        int numeroQuartos;
+        int numeroPavimentos;
+        float areaTerreno;
+        float areaConstruida;
+
+    public:
+        void cadastraCasa();
+};
