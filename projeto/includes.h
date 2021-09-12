983e676ff54e62b0f2812dbfb4a777deaafc5894
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-09-12 16:14:58 -0300

    .

--- projeto/includes.h
+++ projeto/includes.h
@@ -8,3 +8,8 @@
 //#include "consultaVenda.h"
 //#include "consultaAluguel"
 //#include "removeImovel.h"
+
+
+obs: 
+aqui não estão todas as opções do menu, pois ainda estamos decidindo quais estarão presentes em nosso programa
+caso ocorra mudança, devemos lembrar de mudar os números no switch case para indicar as ações correspondentes
